bd48618c59d0f31e9bc0f91aa2ada34e7c103736
Sahil Maheshwari <maheshwari.sahil1@gmail.com>
2019-07-30 22:16:41 -0700

    Fixed linting errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,8 +31,8 @@ class ModelTests(TestCase):
     def test_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-        'test@londonappdev.com',
-        'test123'
+         'test@londonappdev.com',
+         'test123'
         )
 
         self.assertTrue(user.is_superuser)
